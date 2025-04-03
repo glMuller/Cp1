58f4ba7938a71d4faae38273e219fd07edab8176
glMuller <glmuller2001@gmail.com>
2025-04-03 17:44:53 -0300

    Projeto do checkpoint publicado

---
+++ src/br/com/fiap/bean/DespesaFamiliar.java
@@ -0,0 +1,23 @@
+package br.com.fiap.bean;
+// Gabriel Lourenço Müller RM561995
+// André Emygdio Ferreira RMXXXXXX
+// João Victor RMXXXXXX
+public class DespesaFamiliar {
+    //atributos
+    public double rendaFamiliar;
+    public int numeroDeMoradores;
+    public double gastoComLuz;
+    public double gastoComAgua;
+    public double gastoComInternet;
+    public double valorMensalidadeAcademia;
+    public double rendaFamiliarLiquida;
+    //metodos
+    public double calcularTotalDespesas(){
+        return (numeroDeMoradores * valorMensalidadeAcademia) + gastoComAgua + gastoComInternet + gastoComLuz;
+    }
+    public double calcularRendaFamiliarLiquida(){
+        double gastoTotal = calcularTotalDespesas();
+        double rendafamiliarLiquida = rendaFamiliar - gastoTotal;
+        return rendafamiliarLiquida;
+    }
+}
---
+++ src/br/com/fiap/main/Main.java
@@ -0,0 +1,45 @@
+package br.com.fiap.main;
+
+import br.com.fiap.bean.DespesaFamiliar;
+
+import java.util.Scanner;
+
+// Gabriel Lourenço Müller RM561995
+// André Emygdio Ferreira RMXXXXXX
+// João Victor RMXXXXXX
+
+public class Main {
+    public static void main(String[] args) {
+        DespesaFamiliar despesa = new DespesaFamiliar();
+        double rendaFamiliar, gastoComLuz, gastoComAgua, gastoComInternet, valorMensalidadeAcademia;
+        int numeroDeMoradores;
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite a renda familiar bruta: ");
+            rendaFamiliar = scan.nextDouble();
+            System.out.println("Digite o número de moradores; ");
+            numeroDeMoradores = scan.nextInt();
+            System.out.println("Digite o valor da conta de luz: ");
+            gastoComLuz = scan.nextDouble();
+            System.out.println("Digite o valor da conta de Água: ");
+            gastoComAgua = scan.nextDouble();
+            System.out.println("Digite o valor da conta de Internet: ");
+            gastoComInternet = scan.nextDouble();
+            System.out.println("Digite o valor da mensalidade da academia: ");
+            valorMensalidadeAcademia = scan.nextDouble();
+           //atribuir valores aos objetos
+            despesa.rendaFamiliar = rendaFamiliar;
+            despesa.numeroDeMoradores = numeroDeMoradores;
+            despesa.gastoComLuz = gastoComLuz;
+            despesa.gastoComAgua = gastoComAgua;
+            despesa.gastoComInternet = gastoComInternet;
+            despesa.valorMensalidadeAcademia = valorMensalidadeAcademia;
+            //exibindo os valores e resultados
+            System.out.printf("renda bruta familiar: R$ %.3f \nGasto da conta de luz: %.3f \nGasto da conta de Água: %.3f \nGasto com conta de Internet %.3f \nRenda familiar líquida: %.3f \nGasto com a mensalidade da academia: %.3f", despesa.rendaFamiliar, despesa.gastoComLuz, despesa.gastoComAgua, despesa.gastoComInternet, despesa.calcularRendaFamiliarLiquida(), despesa.valorMensalidadeAcademia);
+        } catch (Exception e) {
+            System.out.println("Formato de número incorreto");
+            throw new RuntimeException(e);
+        }
+    }
+
+}
